f329532c8b397c9a6409950991cb9f962ec17b66
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-03-23 02:31:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  *  自定义网站配置 
  */
 const config = {
-  title: "自定义导航",                 //write your website title
-  subtitle: "Cloudflare Workers Dir", //write your website subtitle
-  logo_icon: "sitemap",               //select your logo by semantic-ui icon (you can get more msg in:https://semantic-ui.com/elements/icon.html)
+  title: "度娘",                 //write your website title
+  subtitle: "aaaaaaaaa", //write your website subtitle
+  logo_icon: "paper plane",   //(you can get more msg in:https://semantic-ui.com/elements/icon.html)
   hitokoto: true,                     //use hitokoto or not
   search:true,                        //enable search function
   search_engine:[                     //choose search engine which you use
@@ -27,13 +27,13 @@ const config = {
   ],
   selling_ads: true,                  //Selling your domain or not.(turning on may be helpful for selling this domain by showing some ads.)
   sell_info:{
-    domain:"example.com",
+    domain:"377020945.xyz",
     price:500,                        //domain price
     mon_unit:"yen sign",              //monetary unit 
     contact:[                         //how to contact you
       {
-        type:"envelope",               //contact type ("weixin","qq","telegram plane","envelope" or "phone")
-        content:"info@example.com"
+        type:"qq",               //contact type ("weixin","qq","telegram plane","envelope" or "phone")
+        content:"377020945@qq.com"
       }
     ]                        
   },
@@ -89,7 +89,241 @@ const config = {
           desc:"程序员集散地"
         },
       ]
-    }
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        }, 
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://w3school.com.cn/",
+          name:"W3school在线教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://runoob.com/",
+          name:"菜鸟教程",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://segmentfault.com/",
+          name:"思否社区",
+          desc:"程序员集散地"
+        },
+        {
+          url:"https://jianshu.com/",
+          name:"简书",
+          desc:"程序员集散地"
+        },
+      ]
+    },
   ]
 }
 const el = (tag, attrs, content) => `<${tag} ${attrs.join(" ")}>${content}</${tag}>`;
@@ -112,10 +346,10 @@ addEventListener('fetch', event => {
 function getFavicon(url){
   if(url.match(/https{0,1}:\/\//)){
     //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url.split('//')[1];
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=" + url;
+    return "https://www.google.com/s2/favicons?sz=64&domain_url=" + url;
   }else{
     //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url;
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=http://" + url;
+    return "https://www.google.com/s2/favicons?sz=64&domain_url=http://" + url;
   } 
 }
 
@@ -124,7 +358,7 @@ function getFavicon(url){
  */
 
 function renderIndex(){
-  const footer = el('footer',[],el('div',['class="footer"'],'Powered by' + el('a',['class="ui label"','href="https://github.com/sleepwood/cf-worker-dir"','target="_blank"'],el('i',['class="github icon"'],"") + 'Cf-Worker-Dir') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'MIT License')));
+  const footer = el('footer',[],el('div',['class="footer"'],'Powered by' + el('a',['class="ui label"','href="https://github.com/hk1996yhx/CF-Worker-DIR"','target="_blank"'],el('i',['class="github icon"'],"") + 'Cf-Worker-Dir') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'MIT License')));
   return renderHeader() + renderMain() + footer;
 }
 
@@ -170,50 +404,98 @@ function renderSeller() {
   var column = el('div',['class="column"'],el('h3',['class="ui center aligned icon inverted header"'],el('i',['class="circular envelope open outline grey inverted icon"'],"") + '联系我') + el('div',['class="ui relaxed celled large list"'],contact));
   var price = el('div',['class="column"'],el('div',['class="ui large yellow statistic"'],el('div',['class="value"'],el('i',[`class="${config.sell_info.mon_unit} icon"`],"") + config.sell_info.price)));
   var content = el('div',['class="content"'],el('div',['class="ui basic segment"'],el('div',['class="ui two column stackable center aligned grid"'],el('div',['class="ui inverted vertical divider"'],'感兴趣？') + el('div',['class="middle aligned row"'],price + column))));
-
+  
   return el('div',['id="seller"','class="ui basic modal"'],title + content + action);
-}
+} 
 
-function renderHTML(index,seller) {
+function renderHTML(index, seller) {
+  // Function to generate a random hexadecimal color
+  function randomHexColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  // Function to generate a random background image URL
+  function randomBackgroundImage() {
+    // Array of sample image URLs
+    const backgrounds = [
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img1.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img2.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img3.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img4.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img5.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img6.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img7.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img8.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img9.jpg)',
+      'url(https://raw.githubusercontent.com/hk1996yhx/CF-Worker-DIR/master/top_img10.jpg)',
+      // Add more image URLs as needed
+    ];
+
+    // Choose a random image URL from the array
+    return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  }
+
+  // Randomly choose between color or image background
+  const useColorBackground = Math.random() <-1; // 50% chance for each
+  const headbackground = useColorBackground ? randomHexColor() : randomBackgroundImage();
+  const bodybackground = useColorBackground ? randomHexColor() : randomBackgroundImage();
+  const colorbackground =   randomHexColor() ;
+
   return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>${config.title} - ${config.subtitle}</title>
-      <link href="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.css" rel="stylesheet">
-      <link href="https://cdn.jsdelivr.net/gh/sleepwood/cf-worker-dir@0.1.1/style.css" rel="stylesheet">
-      <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.js"></script>
-  </head>
-  <body>
-    ${index}
-    ${config.selling_ads ? seller : ''}
-    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
-    <script>
-      $('#sengine a').on('click', function (e) {
-        $('#sengine a.active').toggleClass('active');
-        $(e.target).toggleClass('active');
-        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
-      });
-      $('.search').on('click', function (e) {
-          var url = $('#sengine a.active').data('url');
-          url = url.replace(`+/\$s/+`,$('#searchinput').val());
-          window.open(url);
-      });
-      /* 鼠标聚焦时，回车事件 */
-      $("#searchinput").bind("keypress", function(){
-          if (event.keyCode == 13){
-          // 触发需要调用的方法
-          $(".search").click();
-          }
-      });
-      $('#menubtn').on('click', function (e) {
-          $('#seller').modal('show');
-      });
-    </script>
-  </body>
-
-  </html>`
+<html lang="en">
+<head> 
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>${config.title} - ${config.subtitle}</title> 
+<link href="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.css" rel="stylesheet">
+<link href="https://loader.377020945.workers.dev/style.css" type="text/css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6/dist/jquery.min.js"></script>
+<style> 
+  #head {
+    background: ${headbackground}; 
+    height: 700px; /* 设置背景高度为100像素 */
+  }
+  h1 {
+    background: ${colorbackground};
+  }
+  #sengine {
+    background-color: ${colorbackground};
+    color: white;  
+    padding: 10px; 
+  }
+  main {
+    background: ${bodybackground};
+  } 
+</style>
+</head>
+<body>
+${index}
+${config.selling_ads ? seller : ''}
+<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+<script>
+$('#sengine a').on('click', function (e) {
+  $('#sengine a.active').toggleClass('active');
+  $(e.target).toggleClass('active');
+  $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
+});
+$('.search').on('click', function (e) {
+  var url = $('#sengine a.active').data('url');
+  url = url.replace(`+/\$s/+`,$('#searchinput').val());
+  window.open(url);
+});
+/* 鼠标聚焦时，回车事件 */
+$("#searchinput").bind("keypress", function(){
+  if (event.keyCode == 13){
+    // 触发需要调用的方法
+    $(".search").click();
+  }
+});
+$('#menubtn').on('click', function (e) {
+  $('#seller').modal('show');
+});
+</script>
+</body>
+</html>`;
 }
